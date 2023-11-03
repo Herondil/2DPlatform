bdc1aefba8b6c8a1e9b7a7a2799ceb0aec70b910
Maxens Dubois <maxens.dubois@gmail.com>
2023-11-03 16:30:12 +0100

    Add a movement script

---
+++ Assets/Movement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody2D rgbdref;
+    private Vector2 dir;
+    public float speed;
+
+    private void Awake()
+    {
+        rgbdref = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        dir = new Vector2(horizontal, 0).normalized;
+
+        Debug.DrawLine(rgbdref.position, dir*5, Color.red);
+
+        if(dir.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1,1);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 d = new Vector2(dir.x, rgbdref.velocity.y).normalized;
+        rgbdref.velocity = d*speed;
+    }
+}
